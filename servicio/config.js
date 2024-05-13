837fa23fd5e75ef1053aaced656ae1b56d69aa45
ice01man <reta.paulo@gail.com>
2024-05-13 15:55:37 -0300

    v0.0.2

--- servicio/config.js
+++ servicio/config.js
@@ -1,11 +1,15 @@
 // mongod --dbpath="./" -> para iniciar el servicio de MongoDB
 //mongosh               -> para iniciar a trabajar con el Servidor MDB 
+import dotenv from 'dotenv'
 
-const MODO_PRESISTENCIA = 'MONGODB'  // FILE - MEM - BASEDATOS
+dotenv.config()
+
 const PORT = process.env.PORT || 8080
-//const STRCNX = 'mongodb://127.0.0.1'
-const STRCNX = 'mongodb+srv://Pauleta:Ikarus135@ecommerce.cbs49zr.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce'
-const BASE = 'ecommerce' 
+
+const MODO_PRESISTENCIA = process.env.MODO_PRESISTENCIA || 'MONGODB'
+const STRCNX = process.env.STRCNX || 'mongodb://127.0.0.1'
+const BASE = process.env.BASE || 'mongod' 
+
 export default {
     MODO_PRESISTENCIA,
     PORT,
